bd31388b379797c0215ccefd729bd9ba77c4043c
Starrah <starrah@foxmail.com>
2022-08-13 00:14:39 +0800

    添加vite-plugin-checker，和vue-tsc。

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     uni(),
+    checker({
+      vueTsc: true,
+    }),
   ],
 });
